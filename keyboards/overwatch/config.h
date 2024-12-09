87d83d79f5db92b2e8c03f605d7b32d0825f2699
mary3587 <kimm02@ewha.ac.kr>
2024-12-09 12:19:44 +0900

    Update config.h

--- keyboards/overwatch/config.h
+++ keyboards/overwatch/config.h
@@ -18,28 +18,6 @@
 #include QMK_KEYBOARD_H
 
 
-// USB 디바이스 정보
-#define VENDOR_ID       0xFEED  // 제공된 값: 0xFEED
-#define PRODUCT_ID      0x0000  // 제공된 값: 0x0000
-#define DEVICE_VER      0x0001  // 디바이스 버전 (1.0.0에 맞는 버전으로 설정)
-#define MANUFACTURER    "Mary"  // 제공된 값: "Mary"
-#define PRODUCT         "overwatch" // 제공된 값: "overwatch_keyboard"
-
-// 부트로더 설정 (Pro Micro는 기본적으로 "caterina" 부트로더 사용)
-#define BOOTLOADER      caterina
-
-// 매트릭스 크기 (행과 열의 수)
-#define MATRIX_COLS 6
-#define MATRIX_ROWS 4
-
-// 키 매트릭스 핀 설정
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
-#define MATRIX_ROW_PINS { D3, D2, D1, B2 }   // 제공된 값
-   // 제공된 값
-#define DIODE_DIRECTION COL2ROW  // 제공된 값: COL2ROW
-
-
-
 // 디버그 메시지 활성화 (선택 사항)
 #define DEBUG  // 디버그 메시지 활성화
 
